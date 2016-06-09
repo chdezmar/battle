01f6dd0f30cd6404f04cfb35e5af5c4d37298a37
a-mellor <alexander.mellor@gmail.com>
2016-06-09 18:07:46 +0100

    Added the Game spec and lib to start User story 5

---
+++ spec/game_spec.rb
@@ -0,0 +1,10 @@
+require 'game'
+
+describe Game do
+  let(:player) {double (:other_player, damage: => nil)}
+
+  it '#attack calls damage on the parameter' do
+    expect(player).to receive(:damage)
+    described_class.new.attack(player)
+  end
+end
