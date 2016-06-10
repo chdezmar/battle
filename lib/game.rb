f1b33e9d71bbe72f9563ddcdb0c3b9c502d0015e
a-mellor <alexander.mellor@gmail.com>
2016-06-10 12:32:10 +0100

    refactored the game class to initialize with 2 players

--- lib/game.rb
+++ lib/game.rb
@@ -1,9 +1,21 @@
 class Game
 
+  attr_reader :players
+
+  def initialize(player1, player2)
+    @players = [player1, player2]
+  end
 
 	def attack(player)
     player.receive_damage
   end
 
+  def player1
+    players.first
+  end
+
+  def player2
+    players.last
+  end
 
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -2,7 +2,7 @@ require 'game'
 
 describe Game do
 
-  subject(:game) { described_class.new}
+  subject(:game) { described_class.new(player1, player2) }
 
   let(:player1) { double :player1 }
   let(:player2) { double :player2 }
@@ -16,10 +16,15 @@ describe Game do
     end
   end
 
-  # describe '#players' do
-  #   it 'starts the game with two players' do
-  #     expect(game.players).to eq players
-  #   end
-  # end
+  describe '#player1' do
+    it 'returns the first player' do
+      expect(game.player1).to eq player1
+    end
+  end
 
+  describe '#player2' do
+    it 'returns the first player' do
+      expect(game.player2).to eq player2
+    end
+  end
 end
